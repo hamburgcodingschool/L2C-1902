67814c589bd4f26d92e17ea4c676c9988634edb5
Helder Pereira <helder.pereira@xing.com>
2019-02-25 18:52:46 +0100

    lesson 8

---
+++ lesson 8/instantiate.js
@@ -0,0 +1,16 @@
+// let currentDate = new Date(); // #238746
+
+class Person {
+   constructor(name, age, city) {
+       this.name = name;
+       this.age = age;
+       this.city = city;
+   }
+}
+
+const l2cCourse = [
+    new Person("Kat", 27, "HH"),
+    new Person("Patrick", 32, "HH"),
+    new Person("Aneesh", 31, "HH"),
+    new Person("Helder", 38, "HH")
+];
